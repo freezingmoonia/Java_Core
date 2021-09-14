d1563836644517084c770c37e87bd6113b10b449
Magadeeva GR <freezingmoonia@protonmail.com>
2021-09-14 22:32:49 +0300

    готовое дз lesson_3

---
+++ Lesson_3_java_Core/Fruit.java
@@ -0,0 +1,23 @@
+package Lesson_3_java_Core;
+
+public class Fruit {
+
+    public float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+
+
+    public Fruit() {
+    }
+}
